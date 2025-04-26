ab50eda7ad74d67ed5202f219f1f577797b05ad7
Pedro Delgadillo <91334249+petedillo@users.noreply.github.com>
2025-04-25 22:16:52 -0500

    update server with jwt check and add env var for port

--- server.js
+++ server.js
@@ -7,8 +7,14 @@ const exercisesRouter = require('./routes/exercises');
 const workoutsRouter = require('./routes/workouts');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const prisma = new PrismaClient();
+
+// Make sure JWT_SECRET is set
+if (!process.env.JWT_SECRET) {
+  console.error('JWT_SECRET environment variable not set!');
+  process.exit(1);
+}
 
 app.use(express.json());
 
